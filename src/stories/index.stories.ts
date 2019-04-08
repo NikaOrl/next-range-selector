acd3cbb0b1cea3550598a8abd038a6182bf61a12
Nika Orlova <nika.orlova.1997@yandex.ru>
2019-04-09 01:51:58 +0300

    storybook generated

---
+++ src/stories/index.stories.ts
@@ -0,0 +1,6 @@
+import {storiesOf} from '@storybook/angular';
+// import { Next * ComponentName * Component; } from; 'next-*component-name*';
+import {withNotes} from '@storybook/addon-notes';
+import {withKnobs} from '@storybook/addon-knobs';
+import * as marked from 'marked';
+import defaultText from './default.md';
