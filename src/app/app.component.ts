1fee2035ee543112497d35207679b0156abe7d33
Nika Orlova <nika.orlova.1997@yandex.ru>
2019-04-14 17:32:03 +0300

    marks, mark-template and data added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,13 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   public title = 'next-range-selector-project';
-  public value = 10;
+  public value = 'b';
   public value2 = [10, 40];
   public value3 = [10, 40];
   public value4 = [10, 40];
   public value5 = [10, 40];
   public borders = [{max: 30, min: 5}, {max: 50, min: 30}];
   public dotOptions = [{disabled: false}, {disabled: true}];
+  public data = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+  public marks = [0, 10, 40, 60, 80, 100];
 }
